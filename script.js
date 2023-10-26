85490cfadb414d112f7769949a2177944ac3a501
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-26 11:05:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const deleteButton = document.querySelector("#delete");
+
+deleteButton.addEventListener("click", ()=>{
+	const dropDown = document.getElementById("colorSelect");
+	const selected = dropDown.options[dropDown.selectedIndex];
+	if(selected){
+		dropDown.removeChild(selected);
+	}
+})
+
+
